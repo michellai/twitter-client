e8fbe02c41304aa2c88669f52f0287c235de6651
Michelle Lai <michellai@gmail.com>
2014-04-21 23:07:09 -0700

    Better scrolling fetch, reset view, but sort is still off

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -61,7 +61,7 @@ var tweetCollection = Backbone.Collection.extend({
       var twre = /\@([a-z]+)/ig;
       var twhash = /\#([a-z]+)/ig;
       var twurl = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-      console.log(statusText);
+      //console.log(statusText);
       var newTxt = statusText.replace(twurl, '<a href="$1">$1</a>')
                            .replace(twre, '<a href="http://twitter.com/@$1">@$1</a>')
                            .replace(twhash, '<a href="http://search.twitter.com/search?q=$1">#$1</a>');
@@ -73,35 +73,42 @@ var tweetCollection = Backbone.Collection.extend({
       }
     },
     sortByRetweeted: function () {
-      this.setMultiplier();
-      //debugger
+      //this.setMultiplier();
+      console.log('sorting now....multiplier '+this.multiplier)
+      
+      //this.comparator = 'retweet_count';
       this.sort(function (a, b) {
-          
-          if (a.attributes.retweet_count > b.attributes.retweet_count)
-              return this.multiplier*-1;
-          if (a.attributes.retweet_count < b.attributes.retweet_count)
+          if (a.attributes.retweet_count > b.attributes.retweet_count) {
+              console.log(a+' '+b);
               return this.multiplier*1;
+          }
+          if (a.attributes.retweet_count < b.attributes.retweet_count)
+          {
+              debugger
+              console.log(a+' '+b);
+              return this.multiplier*-1;
+          }
           return 0;
-      });
+      }); 
     },
     sortByLocations: function () {
 
       this.sort(function (a, b) {
-          if (a.attributes.user.followers_count > b.attributes.user.followers_count)
-              return -1;
           if (a.attributes.user.followers_count < b.attributes.user.followers_count)
+              return -1;
+          if (a.attributes.user.followers_count > b.attributes.user.followers_count)
               return 1;
           return 0;
       });
     },
     sortByFollowers: function () {
-      this.setMultiplier();
-
+      //this.setMultiplier();
+      //debugger
       this.sort(function (a, b) {
           if (a.attributes.user.followers_count > b.attributes.user.followers_count)
               return this.multiplier*-1;
           if (a.attributes.user.followers_count < b.attributes.user.followers_count)
-              return 1;
+              return this.multiplier*1;
           return 0;
       });
     },
@@ -123,8 +130,16 @@ var sortview = Backbone.View.extend({
       'click .retweet-button': 'sortByRetweeted',
       'click .followers-button': 'sortByFollowers'
   },
+  sortByRetweeted: function() {
+      this.collection.multiplier = -1*this.collection.multiplier;
+      this.collection.sortByRetweeted();
+  },
+  sortByFollowers: function() {
+      this.collection.multiplier = -1*this.collection.multiplier;
+      this.collection.sortByFollowers();
+  }
 
-})
+});
 var testview = Backbone.View.extend({ 
   el: '#backbonetest',
   lastRendered: 0,
@@ -143,56 +158,64 @@ var testview = Backbone.View.extend({
 
   render: function() {
       
-      if (this.lastComparator != this.collection.comparator) {
-        this.resetView();
-        this.lastComparator = this.collection.comparator;
-      }
+      this.resetView();
+      $('body').removeClass('processing');
       this.buildList();
+      console.log('removing processing tag')
       
+
   },
   resetView: function() {
       $('.list').html('');
+      this.lastRendered = 0;
   },
   errorMsg: function() {
       console.log("could not find");
   },
   buildList: function() {
-      debugger
-      console.log('building list');
-      if (this.lastRendered < this.collection.models.length) {
-        this.resetView();
+      //debugger
+
+      console.log('building list '+this.lastRendered + ' ' + this.collection.models.length);
+
+      if (this.lastRendered <= this.collection.models.length) {
+        
         var start = this.lastRendered;
-        console.log("LAST TWEET RENDERED: ", start);
+        //console.log("LAST TWEET RENDERED: ", start);
+        
         for(var i = start; i< this.collection.models.length; i++) {
             $('.list').append(JST.tweet({tweet:this.collection.models[i].attributes}));
             
-            console.log('adding to view')
+            console.log('XXXXX adding index: '+i+' view')
             this.lastRendered += 1;
         }
-        $('body').removeClass('processing');
+        //debugger
+        /*
+        for(var i = 0; i< this.collection.models.length; i++) {
+            $('.list').append(JST.tweet({tweet:this.collection.models[i].attributes}));
+        }*/
       }      
   },
   checkScroll: function () {
       var triggerPoint = 100; // 100px from the bottom
       if( !$('body').hasClass('processing')) {
-          console.log('dont see any process');
+          //console.log('dont see any process');
           if($(window).scrollTop() + $(window).height() >= .9*$(document).height()) {
-            $('body').addClass('processing');
-            console.log('adding processing tag');
-            this.collection.fetch( {
-                data: { page : this.collection.pageNum },
-                remove: false,
-            });
+              $('body').addClass('processing');
+              console.log('adding processing tag');
+              this.collection.fetch( {
+                  data: { page : this.collection.pageNum },
+                  remove: false,
+              });
             
 
-            $('.rtlist').html('');
-            $('.followlist').html('');
-            $('.locationlist').html('');
-            //not working, help.
-            $("img").on('error', function() {
-              $(this).attr('src', 'http://placekitten.com/52/52');
-            });
-            this.collection.pageNum += 1;
+              $('.rtlist').html('');
+              $('.followlist').html('');
+              $('.locationlist').html('');
+              //not working, help.
+              $("img").on('error', function() {
+                $(this).attr('src', 'http://placekitten.com/52/52');
+              });
+              this.collection.pageNum += 1;
           }
           console.log('PAGE: ', this.collection.pageNum);
           
